d04b59a57c72af6cd5d6477db10bb153f238bf03
dmatseku <45592892+dmatseku@users.noreply.github.com>
2024-02-16 01:57:16 +0200

    maybe not working version

--- Modules/ConfigRegistration.cs
+++ Modules/ConfigRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.API.Interfaces;
+using ForbidToOpenLockedDoorsConfig = ChickenDinnerV2.Modules.ForbidToOpenLockedDoors.Config;
 
 namespace ChickenDinnerV2.Modules
 {
@@ -6,5 +7,7 @@ namespace ChickenDinnerV2.Modules
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
+
+        public ForbidToOpenLockedDoorsConfig ForbidToOpenLockedDoors { get; set; } = new ForbidToOpenLockedDoorsConfig();
     }
 }
