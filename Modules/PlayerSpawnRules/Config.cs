ae7e218ccf96b65b3d9476a21a7e5ddfd5727535
dmatseku <45592892+dmatseku@users.noreply.github.com>
2024-02-17 13:54:12 +0200

    structure of the mod

--- Modules/PlayerSpawnRules/Config.cs
+++ Modules/PlayerSpawnRules/Config.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using ChickenDinnerV2.Core.Interfaces;
+using System.ComponentModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChickenDinnerV2.Modules.PlayerSpawnRules
 {
-    internal class Config
+    public class Config : IModuleConfig
     {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("Roles order on round start. several variants separated by coma")]
+        public List<string> spawnOrder { get; set; } = new List<string>()
+        {
+            "ClassD",
+            "Scp096, Scp173, Scp106, Scp049, Scp079, Scp939, Scp3114"
+        };
+
+        [Description("Roles order on round start. several variants separated by coma")]
+        public Dictionary<string, Dictionary<string, Dictionary<string, int>>> RoleItems { get; set; } = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>()
+        {
+            {
+                "Scientist", new Dictionary<string, Dictionary<string, int>>
+                {
+                    {
+                        "Scientist", new Dictionary<string, int>
+                        {
+                            { "KeycardScientist", 1 }
+                        }
+                    },
+                    {
+                        "ZoneManager", new Dictionary<string, int>
+                        {
+                            { "KeycardZoneManager", 1 }
+                        }
+                    }
+                }
+            }
+        };
     }
 }
