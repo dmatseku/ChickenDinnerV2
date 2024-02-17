da7f028b7887c4982feb4e7bb34661d0b447c5d2
dmatseku <45592892+dmatseku@users.noreply.github.com>
2024-02-17 14:44:16 +0200

    spawn order implemented

--- Modules/PlayerSpawnRules/Events/Server/RoundStarted.cs
+++ Modules/PlayerSpawnRules/Events/Server/RoundStarted.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChickenDinnerV2.Core;
+using ChickenDinnerV2.Core.Interfaces;
+using ChickenDinnerV2.Modules.PlayerSpawnRules.Model;
+using Exiled.API.Features;
+using Exiled.Events.Handlers;
 
 namespace ChickenDinnerV2.Modules.PlayerSpawnRules.Events.Server
 {
     internal class RoundStarted
     {
+        protected static Config PlayerSpawnRulesConfig = Main.Instance.Config.PlayerSpawnRules;
+
+        public bool Register()
+        {
+            if (PlayerSpawnRulesConfig.IsEnabled)
+            {
+                Exiled.Events.Handlers.Server.RoundStarted += ApplySpawnOrder;
+                return true;
+            }
+            return false;
+        }
+
+        public void Unregister()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= ApplySpawnOrder;
+        }
+
+        public void ApplySpawnOrder()
+        {
+            Log.Warn("hello");
+            bool isRoundLocked = Round.IsLocked;
+            Round.IsLocked = true;
+            SpawnOrder.Apply();
+            Round.IsLocked = isRoundLocked;
+        }
     }
 }
---
+++ Modules/PlayerSpawnRules/Model/SpawnOrder.cs
@@ -0,0 +1,82 @@
+﻿using ChickenDinnerV2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using PlayerRoles;
+using System.Text.RegularExpressions;
+
+namespace ChickenDinnerV2.Modules.PlayerSpawnRules.Model
+{
+    internal static class SpawnOrder
+    {
+        private static Config PlayerSpawnRulesConfig = Main.Instance.Config.PlayerSpawnRules;
+
+        private static Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+        private static List<int> getPlayerList(int count)
+        {
+            List<int> result = new List<int>(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = i;
+            }
+            return result;
+        }
+
+        private static RoleTypeId? GetRoleTypeId(string role, List<RoleTypeId> busyUniqRoles)
+        {
+            List<string> roles = new Regex(@"\s,\s").Split(role).ToList();
+            RoleTypeId result;
+            
+            while (roles.Count > 0)
+            {
+                int RoleId = rand.Next(roles.Count);
+                
+                if (Enum.TryParse(roles[RoleId], out result))
+                {
+                    if (!roles[RoleId].Contains("Scp") || !busyUniqRoles.Contains(result))
+                    {
+                        if (roles[RoleId].Contains("Scp"))
+                        {
+                            busyUniqRoles.Add(result);
+                        }
+                        return result;
+                    }
+                }
+                roles.RemoveAt(RoleId);
+            }
+
+            return null;
+        }
+
+        public static void Apply()
+        {
+            IReadOnlyCollection<Player> PlayerList = Player.List;
+            List<int> ids = getPlayerList(PlayerList.Count);
+            int SpawnOrderId = 0;
+            List<RoleTypeId> busyUniqRoles = new List<RoleTypeId>();
+
+            while (ids.Count > 0)
+            {
+                int playerId = rand.Next(ids.Count);
+                RoleTypeId? roleTypeId = GetRoleTypeId(PlayerSpawnRulesConfig.spawnOrder[SpawnOrderId], busyUniqRoles);
+
+                if (roleTypeId != null)
+                {
+                    PlayerList.ElementAt(playerId).RoleManager.InitializeNewRole((RoleTypeId)roleTypeId, RoleChangeReason.RoundStart);
+                }
+
+                ids.Remove(playerId);
+                SpawnOrderId++;
+                if (SpawnOrderId >= PlayerSpawnRulesConfig.spawnOrder.Count)
+                {
+                    SpawnOrderId = 0;
+                }
+            }
+        }
+    }
+}
