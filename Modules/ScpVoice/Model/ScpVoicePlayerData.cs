473c5f2b381f3ff7effc17daf2e72759c7b08797
dmatseku <45592892+dmatseku@users.noreply.github.com>
2024-03-09 16:58:31 +0200

    continue replace by plugin

---
+++ Modules/ScpVoice/Model/ScpVoicePlayerData.cs
@@ -0,0 +1,26 @@
+﻿using ChickenDinnerV2.Core.Interfaces;
+using Exiled.API.Features;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenDinnerV2.Modules.ScpVoice.Model
+{
+    internal class ScpVoicePlayerData : IPlayerData
+    {
+        public Player Owner { get; set; }
+
+        public void Created()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RoleChanged(RoleTypeId newRole)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Modules/ScpVoice/Plugins/Player/StandardScpVoiceModulePlugin.cs
+++ Modules/ScpVoice/Plugins/Player/StandardScpVoiceModulePlugin.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using HarmonyLib;
+using InventorySystem.Items.Radio;
+using PlayerRoles.PlayableScps;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoiceChat;
 
 namespace ChickenDinnerV2.Modules.ScpVoice.Plugins.Player
 {
+    [HarmonyPatch(typeof(StandardScpVoiceModule))]
     internal class StandardScpVoiceModulePlugin
     {
+        protected static Config ScpVoiceConfig = ChickenDinnerV2.Core.Main.Instance.Config.ScpVoice;
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch("ProcessInputs")]
+        public static bool ProcessInputsPrefix(StandardScpVoiceModule __instance, ref VoiceChatChannel __result, bool primary, bool alt)
+        {
+            if (primary)
+            {
+                __result = VoiceChatChannel.ScpChat;
+            }
+            else if (alt && __instance.IsProximity)
+            {
+                __result = VoiceChatChannel.Proximity;
+            }
+            else
+            {
+                __result = VoiceChatChannel.None;
+            }
+
+            return false;
+        }
     }
 }
