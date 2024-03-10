3b0a57e89979866dc6e2c0d7256c3fa444a9f348
dmatseku <45592892+dmatseku@users.noreply.github.com>
2024-03-10 20:06:29 +0200

    team config

---
+++ Modules/PlayerSpawnRules/Model/TeamGeneralConfig.cs
@@ -0,0 +1,31 @@
+﻿using ChickenDinnerV2.Core;
+using Exiled.Events.EventArgs.Server;
+using Respawning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenDinnerV2.Modules.PlayerSpawnRules.Model
+{
+    internal static class TeamGeneralConfig
+    {
+        private static Config PlayerSpawnRulesConfig = Main.Instance.Config.PlayerSpawnRules;
+
+        public static void ApplyConfig(RespawningTeamEventArgs ev)
+        {
+            Dictionary<string, string> config;
+
+            if (PlayerSpawnRulesConfig.TeamConfig.TryGetValue(ev.NextKnownTeam.ToString(), out config))
+            {
+                int teamSize;
+
+                if (int.TryParse(config["max_team_size"], out teamSize))
+                {
+                    ev.MaximumRespawnAmount = teamSize;
+                }
+            }
+        }
+    }
+}
